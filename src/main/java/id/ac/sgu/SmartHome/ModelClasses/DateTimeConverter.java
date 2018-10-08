0edb9af1707ed9e15d9313d6f3083fe1b130295b
TheFatFatCoder <thefatflyer@gmail.com>
2018-10-08 15:32:03 +0700

    fixed DateTimeGenerator

--- src/main/java/id/ac/sgu/SmartHome/ModelClasses/DateTimeConverter.java
+++ src/main/java/id/ac/sgu/SmartHome/ModelClasses/DateTimeConverter.java
@@ -3,6 +3,7 @@ package id.ac.sgu.SmartHome.ModelClasses;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateTimeConverter {
 	private LocalDateTime dateTime;
@@ -11,7 +12,8 @@ public class DateTimeConverter {
 		String[] hourAndMinute = dateTimeStr.split(":");
 		int hour = Integer.parseInt(hourAndMinute[0]);
 		int minute = Integer.parseInt(hourAndMinute[1]);
-		this.dateTime =  LocalTime.parse(hour+":"+minute).atDate(LocalDate.now());
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_TIME;
+		this.dateTime =  LocalTime.parse(hour+":"+minute, dateTimeFormatter).atDate(LocalDate.now());
 	}
 	
 	public static LocalDateTime convertTime(String dateTimeStr) {
--- src/main/java/id/ac/sgu/SmartHome/ModelClasses/DoorController.java
+++ src/main/java/id/ac/sgu/SmartHome/ModelClasses/DoorController.java
@@ -1,13 +1,29 @@
 package id.ac.sgu.SmartHome.ModelClasses;
 
-import id.ac.sgu.SmartHome.AbstractClasses.AbstractSensor;
+import java.time.LocalDateTime;
+import java.util.Observable;
+import java.util.Observer;
 
-public class DoorController extends AbstractSensor {
+import id.ac.sgu.SmartHome.AbstractClasses.AbstractActuator;
+import id.ac.sgu.SmartHome.AbstractClasses.AbstractController;
+
+public class DoorController extends AbstractController implements Observer{
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
+	
+	public DoorController(LocalDateTime start, LocalDateTime end) {
+		this.startTime = start;
+		this.endTime = end;
+	}
 
 	@Override
-	public void setValue(Object x) {
-		setChanged();
-		notifyObservers();
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private boolean timeWithinOnRange(Object param) {
+		return endTime.isAfter((LocalDateTime) param) && startTime.isBefore((LocalDateTime) param);
 	}
 
 }
