d984828f9410a4df0a2a585480076056302010d4
gwilliam187 <33857496+gwilliam187@users.noreply.github.com>
2018-10-08 13:09:23 +0700

    modified controllers

--- src/main/java/id/ac/sgu/SmartHome/Controller/BlindsController.java
+++ src/main/java/id/ac/sgu/SmartHome/Controller/BlindsController.java
@@ -12,20 +12,30 @@ public class BlindsController extends AbstractController implements Observer {
 	
 	private Blinds blinds;
 	private WindSensor windSensor;
-	
+	private int knotsThreshold;
+
 	public BlindsController(Blinds blinds, WindSensor windSensor) {
 		this.blinds = blinds;
 		this.windSensor = windSensor;
+		this.knotsThreshold = 25;
 	}
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		if((Double) windSensor.getValue() < 25) { 	// Turn Off Blinds
+		if((Double) windSensor.getValue() < knotsThreshold) { 	// Turn Off Blinds
 			blinds.doAction(false, "");
 			view.turnOnBlinds(false);
-		} else {									// Turn On Blinds
+		} else {												// Turn On Blinds
 			blinds.doAction(true, "");
 			view.turnOnBlinds(true);
 		}
+	}
+	
+	public int getKnotsThreshold() {
+		return knotsThreshold;
+	}
+
+	public void setKnotsThreshold(int knotsThreshold) {
+		this.knotsThreshold = knotsThreshold;
 	}
 }
--- src/main/java/id/ac/sgu/SmartHome/Controller/LightsController.java
+++ src/main/java/id/ac/sgu/SmartHome/Controller/LightsController.java
@@ -15,22 +15,41 @@ public class LightsController extends AbstractController implements Observer{
 
 	private Lights lights;
 	private ClockSensor clockSensor;
+	private int onTimeStart;
+	private int onTimeEnd;
 	
 	public LightsController(Lights lights, ClockSensor clockSensor) {
 		this.lights = lights;
 		this.clockSensor = clockSensor;
+		onTimeStart = 7;
+		onTimeEnd = 18;
 	}
 	
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		int hour = ((Time) clockSensor.getValue()).getHours();
-		if(hour > 7 && hour < 18) {		// Turn Off Lights
+		if(hour > onTimeStart && hour < onTimeEnd) {	// Turn Off Lights
 			lights.doAction(false, "");
 			view.turnOnLights(false);
-		} else {						// Turn On Lights
+		} else {										// Turn On Lights
 			lights.doAction(true, "");
 			view.turnOnLights(true);
 		}
 	}
 
+	public int getOnTimeStart() {
+		return onTimeStart;
+	}
+
+	public void setOnTimeStart(int onTimeStart) {
+		this.onTimeStart = onTimeStart;
+	}
+
+	public int getOnTimeEnd() {
+		return onTimeEnd;
+	}
+
+	public void setOnTimeEnd(int onTimeEnd) {
+		this.onTimeEnd = onTimeEnd;
+	}
 }
